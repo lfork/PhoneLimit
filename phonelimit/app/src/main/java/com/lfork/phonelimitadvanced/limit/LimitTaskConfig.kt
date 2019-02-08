f11990f1c90abe7aaf2f3fe8296afa00ddbb7b38
lfork <986204478@qq.com>
2019-02-08 13:40:34 +0800

    rename limit model

--- phonelimit/app/src/main/java/com/lfork/phonelimitadvanced/limit/LimitTaskConfig.kt
+++ phonelimit/app/src/main/java/com/lfork/phonelimitadvanced/limit/LimitTaskConfig.kt
@@ -15,21 +15,26 @@ data class LimitTaskConfig(
         var limitTimeSeconds: Long = -1,
         var periodMillis: Long = -1,
         var isImmediatelyExecuted: Boolean = false,
-        var limitModel:Int = LimitTaskConfig.LIMIT_MODEL_HEAVY_LAUNCHER_LIMIT
+        var limitModel:Int = LimitTaskConfig.LIMIT_MODEL_HEAVY1
 ):Serializable{
     companion object {
-        const val LIMIT_MODEL_LIGHT_APP_JUMP = 0
 
-        const val LIMIT_MODEL_HEAVY_LAUNCHER_LIMIT = 1
+        //_APP_JUMP
+        const val LIMIT_MODEL_LIGHT = 0
 
-        const val LIMIT_MODEL_HEAVY_FLOATING_LIMIT = 2
+        //_LAUNCHER_LIMIT
+        const val LIMIT_MODEL_HEAVY1 = 1
+
+        //_FLOATING_LIMIT
+        const val LIMIT_MODEL_HEAVY2 = 2
 
         /**
          * 在小米等有可以关闭应用程序的手机助手APP的机型上适用
+         * _LAUNCHER_FLOATING_MODEL
          */
-        const val LIMIT_MODEL_ULTIMATE_LAUNCHER_FLOATING_MODEL = 3
+        const val LIMIT_MODEL_ULTIMATE = 3
 
-        const val LIMIT_MODEL_ROOT_MODEL = 4
+        const val LIMIT_MODEL_ROOT = 4
     }
 
 
